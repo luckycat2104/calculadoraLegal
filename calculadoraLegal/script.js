e74d03ec4d1d68f13930357fc6cc600549f30917
Mello Sonza <laramsonza@gmail.com>
2024-07-29 12:40:07 -0300

    sim

---
+++ calculadoraLegal/script.js
@@ -0,0 +1,13 @@
+function Soma(){
+    var somado = parseInt(prompt("Qual numero o senhorio quer somar?"));
+    var somadinho = parseInt(prompt("Qual o outro numero pra somar?"));
+    var resultadoSoma = somado + somadinho;
+    alert(resultadoSoma);
+    }
+
+    function Subtracao(){
+        var sub = parseInt(prompt("Qual numero o senhorio quer subtrair?"));
+        var subzinho = parseInt(prompt("Qual o outro numero pra subtrair?"));
+        var resultadoSub = sub - subzinho;
+        alert(resultadoSub);
+        }
